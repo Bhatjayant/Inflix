91b9a70dd4b38e8ca78b1646d8a986ec471e443b
Bhatjayant <152197742+Bhatjayant@users.noreply.github.com>
2026-06-08 22:27:20 +0530

    Delete components directory

--- components/ContactSection.tsx
+++
@@ -1,8 +0,0 @@
-export default function ContactSection() {
-  return (
-    <section className="py-20 text-center">
-      <h2 className="text-4xl mb-4">Contact Us</h2>
-      <p>inflixpharma@gmail.com | +91 8874438874</p>
-    </section>
-  );
-}
